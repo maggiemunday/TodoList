c0437517fdd7d4820eb7779ec4e1d7a53b8dff5b
Maggie Munday <maggie.munday@willowtreeapps.com>
2022-08-01 09:04:53 -0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Maggie Munday on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
